00020fa5bea52f76723eec61e5e76018ee5151a5
Christian Ofori-Achiaw <christianoforiachiaw@gmail.com>
2021-03-14 11:05:56 +0000

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `pos`.`administrator`(`admin_id`, `full_name`, `username`, `password`, `picture`) VALUES (1, 'admin', 'admin', '2290E425D6ABDBA9765581D5844D21364F2CD40D79D7FB60721C74321881890EB438A178F73172E2EEC53F9FE6B4211BDFC14FB1B5041A1A6673B622123E2BA7', NULL);
+
+INSERT INTO `pos`.`role`(`role_id`, `role`) VALUES (1, 'Manager');
+INSERT INTO `pos`.`role`(`role_id`, `role`) VALUES (2, 'Sales Person');
